b68adbbae7709fea19752f4c849c3e1c9f069243
mak001 <mak001@users.noreply.github.com>
2017-12-15 11:40:40 -0600

    Site config and Segments (#2)
    * Started to add siteconfig options
      - instead of going full config.yml
    
    * Now adds to selected list and segment
    
    * Update button now shows status

---
+++ _config.php
@@ -0,0 +1,3 @@
+<?php
+
+if(!defined('FOXYSTRIPE_MAILCHIMP')) define('FOXYSTRIPE_MAILCHIMP', rtrim(basename(dirname(__FILE__))));
--- code/extensions/FoxyStripeMailChimpExtension.php
+++ code/extensions/FoxyStripeMailChimpExtension.php
@@ -9,8 +9,6 @@ class FoxyStripeMailChimpExtension extends Extension
 {
 
     /**
-     * Modified from fox2chimp/fc-mailchimp.php
-     *
      * @param $dataFeed
      */
     public function addIntegrations($dataFeed)
@@ -18,37 +16,26 @@ class FoxyStripeMailChimpExtension extends Extension
         $FoxyData = rc4crypt::decrypt(FoxyCart::getStoreKey(), $dataFeed);
         $data = simplexml_load_string($FoxyData);
 
-        $apiKey = Config::inst()->get(static::class, 'apikey');
-        $listName = Config::inst()->get(static::class, 'mailing_list_name');
-	    $listID = Config::inst()->get(static::class, 'mailing_list_id');
+	    $config = SiteConfig::current_site_config();
+	    $list = $config->MailingList();
+	    $segment = $config->MailingSegment();
+
+	    $apiKey = Config::inst()->get(static::class, 'apikey');
+	    $listID = $list->MCID;
+	    $segmentID = $segment->MCID;
+
         $useCustomField = Config::inst()->get(static::class, 'use_custom_field');
         $customFieldName = Config::inst()->get(static::class, 'custom_field_name');
         $customFieldValue = Config::inst()->get(static::class, 'custom_field_value');
-        $doubleOpt = Config::inst()->get(static::class, 'double_opt_in');
+        $doubleOpt = $config->DoubleOptIn;
         $emailFormat = 'html';
 
         // if these are not provided error
-        if (!($listName || $listID) || !$apiKey) {
+        if (!$listID || !$apiKey) {
             throw new LogicException('Both the MailChimp api key and the list name or id are required');
         }
 
         $mailChimp = new MailChimp($apiKey);
-
-        if (!$listID) {
-	        $lists = $mailChimp->get('lists');
-	        $listID = -1;
-	        foreach ($lists as $list) {
-		        if (isset($list[0]['name']) && isset($list[0]['id'])) {
-			        if ($list[0]['name'] == $listName) {
-				        $listID = $list[0]['id'];
-			        }
-		        }
-	        }
-        }
-
-        if ($listID === -1) {
-            throw new LogicException('No list with that name was found');
-        }
 
         foreach ($data->transactions->transaction as $tx) {
             $subscribe = !$useCustomField;
@@ -62,7 +49,9 @@ class FoxyStripeMailChimpExtension extends Extension
 
 
             if ($subscribe) {
-                $response = $mailChimp->post("lists/$listID/members", [
+            	$batch = $mailChimp->new_batch();
+
+                $batch->post("add_user", "lists/$listID/members", [
                     // cast email_address to a string so its not a SimpleXMLElement object
                     'email_address' => (string) $tx->customer_email,
                     'merge_fields' => [
@@ -72,8 +61,14 @@ class FoxyStripeMailChimpExtension extends Extension
                     'status' => $doubleOpt ? 'pending' :'subscribed',
                     'email_type' => $emailFormat,
                 ]);
-                // TODO - do something with response?
-	            // print_r($response);
+
+            	if ($segmentID) {
+            		$batch->post("add_user_to_segment", "lists/$listID/segments/$segmentID/members", [
+            			'email_address' => (string) $tx->customer_email,
+		            ]);
+	            }
+
+	            $result = $batch->execute();
             }
         }
 
